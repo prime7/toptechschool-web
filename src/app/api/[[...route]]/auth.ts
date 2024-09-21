1dc6bc9ee5dbb8052484f75037bec7c615764203
Tarek Ahmed <tahmed@chegg.com>
2024-09-21 02:32:23 -0600

    A

--- src/app/api/[[...route]]/auth.ts
+++ src/app/api/[[...route]]/auth.ts
@@ -1,29 +1,29 @@
-import { Hono } from "hono";
-import { handlers } from "@/lib/auth";
-import { NextRequest } from "next/server";
+// import { Hono } from "hono";
+// import { handlers } from "@/lib/auth";
+// import { NextRequest } from "next/server";
 
-const authApp = new Hono();
+// const authApp = new Hono();
 
-authApp.all("/*", async (c) => {
-  const method = c.req.method;
+// authApp.all("/*", async (c) => {
+//   const method = c.req.method;
 
-  const nextRequest = new NextRequest(c.req.url, {
-    method: method,
-    headers: c.req.raw.headers,
-    body:
-      method !== "GET" && method !== "HEAD"
-        ? await c.req.arrayBuffer()
-        : undefined,
-  });
+//   const nextRequest = new NextRequest(c.req.url, {
+//     method: method,
+//     headers: c.req.raw.headers,
+//     body:
+//       method !== "GET" && method !== "HEAD"
+//         ? await c.req.arrayBuffer()
+//         : undefined,
+//   });
 
-  const response = await (method === "GET" ? handlers.GET : handlers.POST)(
-    nextRequest
-  );
+//   const response = await (method === "GET" ? handlers.GET : handlers.POST)(
+//     nextRequest
+//   );
 
-  return new Response(response.body, {
-    status: response.status,
-    headers: response.headers,
-  });
-});
+//   return new Response(response.body, {
+//     status: response.status,
+//     headers: response.headers,
+//   });
+// });
 
-export default authApp;
+// export default authApp;
--- src/app/api/[[...route]]/route.ts
+++ src/app/api/[[...route]]/route.ts
@@ -1,12 +1,12 @@
 import { Hono } from "hono";
 import { handle } from "hono/vercel";
-import authApp from "./auth";
+// import authApp from "./auth";
 import basicApp from "./basic";
 import resumeApp from "./resume";
 
 const app = new Hono().basePath("/api");
 
-app.route("/auth", authApp);
+// app.route("/auth", authApp);
 app.route("/", basicApp);
 app.route("/resume", resumeApp);
 
