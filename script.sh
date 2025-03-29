661698bd286b4462daba9bea31547aba9efd2780
prime7 <tarek5701@gmail.com>
2025-03-29 13:11:40 -0600

    TRY

--- script.sh
+++ script.sh
@@ -2,12 +2,10 @@
 
 echo "VERCEL_GIT_COMMIT_REF: $VERCEL_GIT_COMMIT_REF"
 if [[ "$VERCEL_GIT_COMMIT_REF" == "main"  ]] ; then
-  # Don't build
-  echo "🛑 - Build cancelled"
+  echo "✅ - Build can proceed"
   exit 0;
 
 else
-  # Proceed with the build
-    echo "✅ - Build can proceed"
+  echo "🛑 - Build cancelled"
   exit 1;
 fi
