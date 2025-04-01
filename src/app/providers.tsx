c43bad060d013b39ab3feacfb9cf79fb5baf123d
prime7 <tarek5701@gmail.com>
2025-04-01 15:32:55 -0600

    added google analytics

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -5,12 +5,24 @@ import { SessionProvider } from "next-auth/react";
 import { ThemeProvider } from "next-themes";
 import { Analytics } from "@vercel/analytics/react";
 import { SpeedInsights } from "@vercel/speed-insights/next";
+import ReactGA from "react-ga4";
+import { useEffect } from "react";
+import { usePathname } from "next/navigation";
 
+ReactGA.initialize(process.env.GOOGLE_ANALYTICS_ID || "");
 interface Props {
   children?: React.ReactNode;
 }
 
 export const Providers = ({ children }: Props) => {
+  const pathname = usePathname();
+  useEffect(() => {
+    ReactGA.send({
+      hitType: "pageview",
+      page: pathname,
+    });
+  }, [pathname]);
+
   return (
     <SessionProvider>
       <ThemeProvider
