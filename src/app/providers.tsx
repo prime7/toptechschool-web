ab1edd1bb1421ab9d8e69572072dbbc2bf1051b3
prime7 <tarek5701@gmail.com>
2025-04-02 00:07:43 -0600

    added next 3rd party library for google analytics

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -4,24 +4,13 @@ import { Toaster } from "@/components/ui/toaster";
 import { SessionProvider } from "next-auth/react";
 import { ThemeProvider } from "next-themes";
 import { SpeedInsights } from "@vercel/speed-insights/next";
-import ReactGA from "react-ga4";
-import { useEffect } from "react";
-import { usePathname } from "next/navigation";
+import { GoogleAnalytics } from "@next/third-parties/google";
 
-ReactGA.initialize(process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_ID || "");
 interface Props {
   children?: React.ReactNode;
 }
 
 export const Providers = ({ children }: Props) => {
-  const pathname = usePathname();
-  useEffect(() => {
-    ReactGA.send({
-      hitType: "pageview",
-      page: pathname,
-    });
-  }, [pathname]);
-
   return (
     <SessionProvider>
       <ThemeProvider
@@ -33,10 +22,10 @@ export const Providers = ({ children }: Props) => {
       >
         {children}
         <Toaster />
-
         {process.env.NODE_ENV === "production" && (
           <>
             <SpeedInsights />
+            <GoogleAnalytics gaId={process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_ID!!} />
           </>
         )}
       </ThemeProvider>
