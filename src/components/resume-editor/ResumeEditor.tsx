a8c3683a10dd47207766eff2f4471a37eb5b243d
prime7 <tarek5701@gmail.com>
2025-05-07 01:07:04 -0600

    resume preview

--- src/components/resume-editor/ResumeEditor.tsx
+++ src/components/resume-editor/ResumeEditor.tsx
@@ -1,8 +1,10 @@
-import React, { useState } from 'react';
+import React, { useRef, useEffect, useMemo } from 'react';
 import { SectionType } from './types';
 import { ResumeProvider } from './context/ResumeContext';
+import { useResume } from './context/ResumeContext';
+import { PDFViewer } from '@react-pdf/renderer';
+import ResumePDFDocument from './ResumePDFDocument';
 import Sidebar from './Sidebar';
-import ResumePreview from './ResumePreview';
 import PersonalInfoEditor from './sections/PersonalInforEditor';
 import SummaryEditor from './sections/SummaryEditor';
 import ExperienceEditor from './sections/ExperienceEditor';
@@ -11,9 +13,9 @@ import SkillsEditor from './sections/SkillsEditor';
 import ProjectsEditor from './sections/ProjectsEditor';
 import CertificationsEditor from './sections/CertificationsEditor';
 import ReferencesEditor from './sections/ReferencesEditor';
-import { usePDF } from 'react-to-pdf';
+import { useDebounce } from '@/hooks/use-debounce';
 
-const sectionComponents: Record<SectionType, React.ComponentType> = {
+const SECTION_COMPONENTS: Record<SectionType, React.ComponentType> = {
   personal: PersonalInfoEditor,
   summary: SummaryEditor,
   experience: ExperienceEditor,
@@ -25,39 +27,55 @@ const sectionComponents: Record<SectionType, React.ComponentType> = {
 };
 
 export default function ResumeEditor() {
-  const [activeSection, setActiveSection] = useState<SectionType>('personal');
-  const [isExporting, setIsExporting] = useState(false);
-  const ActiveSectionComponent = sectionComponents[activeSection];
-  const { toPDF, targetRef } = usePDF({
-    filename: 'resume.pdf',
-    page: { format: 'a4', orientation: 'portrait' }
-  });
-
-  const handleExportPDF = async () => {
-    setIsExporting(true);
-    toPDF();
-    setIsExporting(false);
-  };
-
   return (
     <ResumeProvider>
-      <div className="flex flex-row h-[calc(100vh-5rem)]">
-        <div>
-          <Sidebar
-            activeSection={activeSection}
-            setActiveSection={setActiveSection}
-            onExportPDF={handleExportPDF}
-          />
-        </div>
-        <div className="flex-1 p-6">
-          <ActiveSectionComponent />
-        </div>
-        <div className="flex-1 overflow-y-scroll">
-          <div ref={targetRef}>
-            <ResumePreview isPrintMode={isExporting} />
-          </div>
-        </div>
-      </div>
+      <ResumeEditorContent />
     </ResumeProvider>
+  );
+}
+
+function ResumeEditorContent() {
+  const [activeSection, setActiveSection] = React.useState<SectionType>('personal');
+  const { state } = useResume();
+  const pdfContainerRef = useRef<HTMLDivElement>(null);
+  const ActiveSectionComponent = SECTION_COMPONENTS[activeSection];
+
+  const { debouncedValue: debouncedState, isLoading } = useDebounce(state, 500);
+
+  useEffect(() => {
+    const style = document.createElement('style');
+    style.innerHTML = `
+      @import url('https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&family=Poppins:wght@400;500;600;700&family=Roboto:wght@400;500;700&family=Open+Sans:wght@400;500;600;700&display=swap');
+    `;
+    document.head.appendChild(style);
+
+    return () => {
+      document.head.removeChild(style);
+    };
+  }, []);
+
+  const [isClient, setIsClient] = React.useState(false);
+
+  useEffect(() => {
+    setIsClient(true);
+  }, []);
+
+  const StablePDFDocument = useMemo(() => (
+    <ResumePDFDocument resumeData={debouncedState} />
+  ), [debouncedState]);
+
+  return (
+    <div className="h-[calc(100vh-5rem)] flex flex-row">
+      <Sidebar
+        activeSection={activeSection}
+        setActiveSection={setActiveSection}
+      />
+      <div className="flex-1 overflow-y-auto p-4">
+        <ActiveSectionComponent />
+      </div>
+      <PDFViewer className="flex-1">
+        {StablePDFDocument}
+      </PDFViewer>
+    </div>
   );
 }
