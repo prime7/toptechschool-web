1c7b77feee1c6607f0f28b14d00b2253850ebbe9
prime7 <tarek5701@gmail.com>
2025-05-30 13:20:08 -0600

    a

--- src/components/resume-editor/sections/index.ts
+++ src/components/resume-editor/sections/index.ts
@@ -1,5 +1,5 @@
 export { default as EducationSection } from './EducationSection';
 export { default as WorkSection } from './WorkSection';
-export { default as PersonalInfoSection } from './PersonalInforSection';
+export { default as PersonalInfoSection } from './PersonalInfoSection';
 export { default as ProjectsSection } from './ProjectsSection';
 export { default as SummarySection } from './SummarySection';
